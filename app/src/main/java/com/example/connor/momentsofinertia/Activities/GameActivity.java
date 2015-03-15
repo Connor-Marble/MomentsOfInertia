1b2d69e26eb534d3f1c57de8b912680b08640670
Connor Marble <Marbleconnor@gmail.com>
2015-03-15 12:59:13 -0400

    made system UI hide to not interfere with game ui

--- app/src/main/java/com/example/connor/momentsofinertia/Activities/GameActivity.java
+++ app/src/main/java/com/example/connor/momentsofinertia/Activities/GameActivity.java
@@ -51,6 +51,9 @@ public class GameActivity extends Activity{
     @Override
     protected void onResume(){
         super.onResume();
+
+        this.getWindow().getDecorView().setSystemUiVisibility(getUIFlags());
+
         gameView.isRunning = true;
         gameView.lastUpdate = System.currentTimeMillis();
     }
@@ -69,5 +72,14 @@ public class GameActivity extends Activity{
 
     }
 
+    private int getUIFlags() {
+        return View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                |View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                |View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                |View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                |View.SYSTEM_UI_FLAG_FULLSCREEN
+                |View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
+
+    }
 
 }
