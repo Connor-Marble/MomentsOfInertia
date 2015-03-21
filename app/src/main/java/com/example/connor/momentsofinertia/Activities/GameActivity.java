7f44b0b5d500025a1ac8a23ca512b89be94c8339
Connor Marble <Marbleconnor@gmail.com>
2015-03-21 17:01:10 -0400

    added header to files

--- app/src/main/java/com/example/connor/momentsofinertia/Activities/GameActivity.java
+++ app/src/main/java/com/example/connor/momentsofinertia/Activities/GameActivity.java
@@ -1,3 +1,22 @@
+/**
+ * Copyright(C) 2015 Connor Marble
+ *
+ * This file is part of the android game Moments of Inertia
+ *
+ * Moments of Inertia is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Moments of Inertia is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Moments of Inertia.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package com.example.connor.momentsofinertia.Activities;
 
 import com.example.connor.momentsofinertia.Game.Entities.BackgroundStar;
@@ -9,7 +28,7 @@ import com.example.connor.momentsofinertia.Game.Entities.Obstacle;
 import com.example.connor.momentsofinertia.Game.Entities.Player;
 import com.example.connor.momentsofinertia.R;
 import com.example.connor.momentsofinertia.util.Vector2D;
-import com.example.connor.momentsofinertia.util.SystemUiHider;
+
 
 
 import android.app.Activity;
@@ -22,12 +41,6 @@ import android.widget.TextView;
 import java.util.Random;
 
 
-/**
- * An example full-screen activity that shows and hides the system UI (i.e.
- * status bar and navigation/system bar) with user interaction.
- *
- * @see SystemUiHider
- */
 public class GameActivity extends Activity{
 
     private GameView gameView;
--- app/src/main/java/com/example/connor/momentsofinertia/Game/Entities/TitleText.java
+++ app/src/main/java/com/example/connor/momentsofinertia/Game/Entities/TitleText.java
@@ -1,3 +1,22 @@
+/**
+ * Copyright(C) 2015 Connor Marble
+ *
+ * This file is part of the android game Moments of Inertia
+ *
+ * Moments of Inertia is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Moments of Inertia is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Moments of Inertia.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package com.example.connor.momentsofinertia.Game.Entities;
 
 import android.graphics.Bitmap;
